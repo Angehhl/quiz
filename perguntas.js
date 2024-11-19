df1bcafadf0ac523ef96a59744db52de2e8a5b79
Angehhl <ap9621608@gmail.com>
2024-11-19 14:51:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'FILME',
+    'Quem era o melhor amigo do Shurek? :)',
+    'O burro Perry!'
+)
+
+criaCartao(
+    'MUNDO',
+    'Quantas cores ha no arco-iris?',
+    '7!'
+)
+
+criaCartao(
+    'SÉRIE',
+    'Quem a Kiara de Outer Banks se relaciona na terceira e quarta temporada?',
+    'JJ Maybank'
+)
+
+criaCartao(
+    'GERAL',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest!'
+)
